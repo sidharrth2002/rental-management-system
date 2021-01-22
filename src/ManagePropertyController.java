a049a444804114b12e78d14eb1b8f72d284d65d7
msnk3141 <76867193+msnk3141@users.noreply.github.com>
2021-01-22 21:01:40 +0800

    Upload photo in addProperty page
    can upload photo in the form

--- src/ManagePropertyController.java
+++ src/ManagePropertyController.java
@@ -1,8 +1,13 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.stage.Window;
 
+import javax.imageio.ImageIO;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,9 +19,12 @@ public class ManagePropertyController extends Controller implements Initializabl
     public TextField projectField;
     public TextField descriptionField;
     public ComboBox typeField;
+    public Button photoChooseButton;
+    public Label photoFileLabel;
     public TextField priceField;
     public CheckBox assignedStatusField;
     public Button submitButton;
+    public File selectedFile; // for property photo
     public static Property propertyManaged;
 
     private static final Pattern DOUBLE_PATTERN = Pattern.compile(
@@ -40,7 +48,25 @@ public class ManagePropertyController extends Controller implements Initializabl
             typeField.setValue(propertyManaged.getType());
             priceField.setText(Double.toString(propertyManaged.getPrice()));
             assignedStatusField.setSelected(propertyManaged.getStatus());
-        }    }
+        }
+    }
+
+    public void photoFileChooser(ActionEvent actionEvent) throws IOException {
+        FileChooser fc = new FileChooser();
+        selectedFile = fc.showOpenDialog(null); // select a file from the FileChooser
+        if (selectedFile != null && ImageIO.read(selectedFile) != null) {
+            Image propertyImage = new Image(selectedFile.toURI().toString());
+            ImageView propertyImageView = new ImageView(propertyImage);
+            propertyImageView.setPreserveRatio(true); // fix image view ratio
+            propertyImageView.setFitHeight(100); // set image view height
+            photoFileLabel.setText(null); // remove the text
+            photoFileLabel.setGraphic(propertyImageView); // set the scaled image
+        }
+        else {
+            photoFileLabel.setGraphic(null); // remove the image
+            photoFileLabel.setText("No photo uploaded"); // set default text
+        }
+    }
 
     public void submitForm(ActionEvent actionEvent) throws IOException {
         Window window = root.getScene().getWindow();
@@ -69,6 +95,11 @@ public class ManagePropertyController extends Controller implements Initializabl
                     "Please enter the type");
             return;
         }
+        if (selectedFile == null || ImageIO.read(selectedFile) == null) {
+            showAlert(Alert.AlertType.ERROR, window, "Incomplete Data!",
+                    "Please upload a photo");
+            return;
+        }
         if(priceField.getText().isEmpty()) {
             showAlert(Alert.AlertType.ERROR, window, "Incomplete Data!",
                     "Please enter the price");
@@ -76,7 +107,7 @@ public class ManagePropertyController extends Controller implements Initializabl
         }
         if(!DOUBLE_PATTERN.matcher(priceField.getText()).matches()) {
             showAlert(Alert.AlertType.ERROR, window, "Incomplete Data!",
-                    "Invaid price format");
+                    "Invalid price format");
             return;
         }
 //        try {
@@ -89,12 +120,16 @@ public class ManagePropertyController extends Controller implements Initializabl
         //if all goes well, make the property using property builder
         //then add to propertySearchFacade
         if(propertyManaged == null) {
+            // [1] copy photo image file to property photo package "./src/data/propertyPhotos"
+            // [2] rename copied image file
+
             Property newProperty = new Property.Builder()
                     .withName(nameField.getText())
                     .withAddress(addressField.getText())
                     .withProject(projectField.getText())
                     .withDescription(descriptionField.getText())
                     .withType((String)typeField.getValue())
+                    // [3] withPhoto(photo.getAbsolutePath())
                     .withPrice(Double.parseDouble(priceField.getText()))
                     .build();
             propertySearchFacade.addProperty(newProperty);
@@ -105,5 +140,6 @@ public class ManagePropertyController extends Controller implements Initializabl
             propertyManaged.setDescription(descriptionField.getText());
             propertyManaged.setType((String)typeField.getValue());
             propertyManaged.setPrice(Double.parseDouble(priceField.getText()));        }
-        }
+    }
+
 }
