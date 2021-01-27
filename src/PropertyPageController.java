85b4c19c1c090c8e652d46bb15b303a4d3626db8
asmsanad <asmsanad@outlook.com>
2021-01-27 16:24:06 +0800

    fixed property page page design and  (manage property) button.

--- src/PropertyPageController.java
+++ src/PropertyPageController.java
@@ -5,6 +5,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -18,28 +19,43 @@ import java.util.ResourceBundle;
 public class PropertyPageController extends Controller implements Initializable {
     public static Property propertyToDisplay;
     private User user = (User) stage.getUserData();
-
+    boolean myunit = false;
 
 
 
     public VBox imageArea;
     public VBox detailsArea;
+    public VBox optionsArea;
     public Text propertyName;
     public Text propertyAddress;
     public Text propertyDescription;
     public Text propertyPrice;
     public Text propertyFacilities;
     public Text Facilities;
-    public Button mproperty;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        Image image = new Image(propertyToDisplay.getPhoto().substring(6, propertyToDisplay.getPhoto().length()), 800, 400, true, true);
+        Image image = new Image(propertyToDisplay.getPhoto().substring(6, propertyToDisplay.getPhoto().length()), 600, 300, false, false);
         imageArea.getChildren().add(new ImageView(image));
 
-        if (user != null) {
+        // check user properties compare it with displayed property page to make sure it's owned by that user
+        if(user != null){
+            ArrayList<Property>  userProperties  = user.getPropertyList();
+        for (Property property: userProperties){
+            if (property.getID().equals(propertyToDisplay.getID())){
+                myunit = true;
+            }else if (user.getUserID().substring(0,1).equals("ad")){ // admin access
+                myunit = true;
+
+            }
+        }
+        }
+
+
+        if (myunit == true ) {
             Button mproperty = new Button("Manage Property");
-            detailsArea.getChildren().add(mproperty);
+            optionsArea.setPadding(new Insets(0, 0, 5, 50));
+            optionsArea.getChildren().add(mproperty);
             mproperty.setOnAction(e -> {
                 ManagePropertyController.propertyManaged = propertyToDisplay;
                 Parent roleChooserfxml = null;
@@ -64,7 +80,7 @@ public class PropertyPageController extends Controller implements Initializable 
             Facilities.setText(facility + "\n");
         }
         detailsArea.setSpacing(7);
-        imageArea.setPadding(new Insets(10, 50, 50, 50));
+        imageArea.setPadding(new Insets(10, 50, 10, 50));
         detailsArea.setPadding(new Insets(10, 50, 50, 50));
     }
 }
